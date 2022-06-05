29535b6ac492b41e11fdef2b2ae3b2d35af066fa
ajithcl <clajith@gmail.com>
2022-06-05 23:24:25 +0530

    search keys words added to config.ini

--- models/ConfigurationSettings.py
+++ models/ConfigurationSettings.py
@@ -8,13 +8,16 @@ from configparser import ConfigParser
 class ConfigurationSettings:
     def __init__(self):
         self._filter_file = ''
-        config_parser = ConfigParser()
+        self.config_parser = ConfigParser()
         try:
-            config_parser.read('config.ini')
-            self._filter_file = config_parser.get("FilterFile", "FileName")
+            self.config_parser.read('config.ini')
+            self._filter_file = self.config_parser.get("FilterFile", "FileName")
         except FileExistsError as file_error:
             return f"Invalid Configuration {file_error.strerror}"
 
     @property
     def filter_file(self):
         return self._filter_file
+
+    def get_search_key_words(self):
+        return self.config_parser.get(section='SearchKeyWords', option='words')
